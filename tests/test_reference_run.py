728ad467d1a2af1767ae1973e43f3169f939b1e6
stnava <stnava@gmail.com>
2022-01-04 11:11:44 -0500

    ENH: document output writing

--- tests/test_reference_run.py
+++ tests/test_reference_run.py
@@ -1,16 +1,14 @@
 import sys, os
 import unittest
-
 os.environ["TF_NUM_INTEROP_THREADS"] = "8"
 os.environ["TF_NUM_INTRAOP_THREADS"] = "8"
 os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = "8"
-
 import tempfile
 import shutil
-
 import antspyt1w
 import antspynet
 import ants
+import re
 
 antspyt1w.get_data(force_download=True)
 fn = antspyt1w.get_data('PPMI-3803-20120814-MRI_T1-I340756', target_extension='.nii.gz' )
@@ -21,6 +19,10 @@ with tempfile.TemporaryDirectory() as temp_dir:
     tempfn=temp_dir+'/apt1wtest'
     testhier = antspyt1w.hierarchical( img, output_prefix=tempfn,
         labels_to_register=None, imgbxt=None, cit168=False, is_test=True, verbose=True)
+    uid = os.path.basename( fn )
+    uid = re.sub(".nii.gz","",uid)
+    outdf = antspyt1w.merge_hierarchical_csvs_to_wide_format( testhier['dataframes'], identifier=uid )
+    outdf.to_csv( tempfn + "_mergewide.csv" )
 
 ##### specialized labeling for hippocampus
 # hippLR = antspyt1w.deep_hippo( img, templateb, 1 )
