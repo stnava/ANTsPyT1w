e84137d76653d9faa1ce54eb2c2970e096b674f6
stnava <stnava@gmail.com>
2022-01-11 22:08:23 -0500

    ENH: update data version

--- antspyt1w/get_data.py
+++ antspyt1w/get_data.py
@@ -33,7 +33,7 @@ from multiprocessing import Pool
 
 DATA_PATH = os.path.expanduser('~/.antspyt1w/')
 
-def get_data( name=None, force_download=False, version=36, target_extension='.csv' ):
+def get_data( name=None, force_download=False, version=37, target_extension='.csv' ):
     """
     Get ANTsPyT1w data filename
 
