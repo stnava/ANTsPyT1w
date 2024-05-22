c7d0513eed709e3804f3bd1ea4720eea366f56a9
stnava <stnava@gmail.com>
2024-05-22 14:53:05 -0400

    ENH: circle ci case

--- tests/test_reference_run.py
+++ tests/test_reference_run.py
@@ -10,36 +10,29 @@ import antspynet
 import ants
 import re
 import pandas as pd
+
+def test_success():
+    assert true
+
+def test_failure():
+    assert 1 + 1 == 3
+
 def is_circleci():
     return os.getenv('CI') == 'true' and os.getenv('CIRCLECI') == 'true'
 
 if is_circleci():
     print("Running in CircleCI environment - cannot handle it")
-    sys.exit(os.EX_OK) # circle ci cant currently handle it
-    
-antspyt1w.get_data()
-fn = antspyt1w.get_data('PPMI-3803-20120814-MRI_T1-I340756', target_extension='.nii.gz' )
-img = ants.image_read( fn )
-
-testingClass = unittest.TestCase( )
-with tempfile.TemporaryDirectory() as temp_dir:
-    tempfn=temp_dir+'/apt1wtest'
-    testhier = antspyt1w.hierarchical( img, output_prefix=tempfn,
-        labels_to_register=None, imgbxt=None, cit168=False, is_test=True, verbose=True)
-    antspyt1w.write_hierarchical( testhier, output_prefix=tempfn )
-    uid = os.path.basename( fn )
-    uid = re.sub(".nii.gz","",uid)
-    outdf = antspyt1w.merge_hierarchical_csvs_to_wide_format( testhier['dataframes'], identifier=uid )
-    outdf.to_csv( tempfn + "_mergewide.csv" )
-
-##### specialized labeling for hippocampus
-# hippLR = antspyt1w.deep_hippo( img, templateb, 1 )
-# testingClass.assertAlmostEqual(
-#    float( hippLR['HLStats']['VolumeInMillimeters'][0]/20000.0 ),
-#    float( 2956.0/20000.0 ), 2, "HLStats volume not close enough")
-
-temp_dir.cleanup()
-
-##### specialized labeling for hypothalamus
-# FIXME hypothalamus
-sys.exit(os.EX_OK) # code 0, all ok
+else:
+    antspyt1w.get_data()
+    fn = antspyt1w.get_data('PPMI-3803-20120814-MRI_T1-I340756', target_extension='.nii.gz' )
+    img = ants.image_read( fn )
+    with tempfile.TemporaryDirectory() as temp_dir:
+        tempfn=temp_dir+'/apt1wtest'
+        testhier = antspyt1w.hierarchical( img, output_prefix=tempfn,
+            labels_to_register=None, imgbxt=None, cit168=False, is_test=True, verbose=True)
+        antspyt1w.write_hierarchical( testhier, output_prefix=tempfn )
+        uid = os.path.basename( fn )
+        uid = re.sub(".nii.gz","",uid)
+        outdf = antspyt1w.merge_hierarchical_csvs_to_wide_format( testhier['dataframes'], identifier=uid )
+        outdf.to_csv( tempfn + "_mergewide.csv" )
+    temp_dir.cleanup()
