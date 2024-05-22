8bc707403226aac26780dc39951dea0a982dc39d
stnava <stnava@gmail.com>
2024-05-22 16:13:18 -0400

    WIP: ...

--- tests/test_reference_run.py
+++ tests/test_reference_run.py
@@ -21,12 +21,13 @@ if os.getenv('CI') == 'true' and os.getenv('CIRCLECI') == 'true':
         img = ants.image_read( fn )
         assert img is not None
     def test_run():
-        fn = antspyt1w.get_data('PPMI-3803-20120814-MRI_T1-I340756', target_extension='.nii.gz' )
-        img = ants.image_read( fn )
-        tempfn=temp_dir+'/apt1wtest'
-        testhier = antspyt1w.hierarchical( img, output_prefix=tempfn,
-                    labels_to_register=None, imgbxt=None, cit168=False, is_test=True, verbose=True)
-        assert testhier is not None
+        with tempfile.TemporaryDirectory() as temp_dir:
+            fn = antspyt1w.get_data('PPMI-3803-20120814-MRI_T1-I340756', target_extension='.nii.gz' )
+            img = ants.image_read( fn )
+            tempfn=temp_dir+'/apt1wtest'
+            testhier = antspyt1w.hierarchical( img, output_prefix=tempfn,
+                        labels_to_register=None, imgbxt=None, cit168=False, is_test=True, verbose=True)
+            assert testhier is not None
 else:
     def test_simple():
         assert os.getenv('CI') != 'true' and os.getenv('CIRCLECI') != 'true'
